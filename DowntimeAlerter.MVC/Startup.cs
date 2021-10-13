00865209b23bef12816679176858d29ce3cf0e66
necipbaser71 <necipbaser71@gmail.com>
2021-10-13 18:13:18 +0300

    commit

--- DowntimeAlerter.MVC/Startup.cs
+++ DowntimeAlerter.MVC/Startup.cs
@@ -76,6 +76,14 @@ namespace DowntimeAlerter.MVC
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
+
+            using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
+            {
+                var context = serviceScope.ServiceProvider.GetRequiredService<DowntimeAlerterDbContext>();
+                context.Database.Migrate();
+                context.Database.EnsureCreated();
+
+            }
         }
     }
 }
